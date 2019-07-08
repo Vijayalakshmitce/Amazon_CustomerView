6a4d4576280abb1bd34f2a701bb3d4cefc08c35a
Vijayalakshmi <vijayalakshmitce@gmail.com>
2019-07-08 00:44:05 -0400

    added manager view

---
+++ bamazonManager.js
@@ -0,0 +1,140 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+//connection declare start
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Sairakshna5*",
+    database: "customerView_DB"
+});
+///connection declare end closure
+//inquirer start here
+inquirer.prompt([{
+    type: "list",
+    name: "managerList",
+    choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+
+}]).then(function (userChoice) {
+    if (userChoice.managerList === "View Products for Sale") {
+
+        connection.query("SELECT item_id,product_name,price,stock_quantity FROM products_custmer", function (error, productRes) {
+            for (var i = 0; i < productRes.length; i++) {
+                var productID = productRes[i].item_id;
+                var productName = productRes[i].product_name;
+                var productPrice = productRes[i].price;
+                var productQuantity = productRes[i].stock_quantity;
+
+                console.log("*************************\n");
+                console.log("Product ID: " + productID);
+                console.log("Product Name: " + productName);
+                console.log("productPrice: " + productPrice);
+                console.log("Product Quantity: " + productQuantity);
+            }
+            //for loop end here    
+        });
+        //query end clousure here
+        connection.end();
+    }
+    ///if end clousure here
+    else if (userChoice.managerList === "View Low Inventory") {
+
+        connection.query("SELECT item_id,product_name,stock_quantity FROM products_custmer where stock_quantity < 5", function (error, inventres) {
+            if (error) throw error;
+            for (var i = 0; i < inventres.length; i++) {
+                var productID = inventres[i].item_id;
+                var productName = inventres[i].product_name;
+                var productQuantity = inventres[i].stock_quantity;
+                var productSold = inventres[i].Sold_item;
+
+                console.log("*************************************\n");
+                console.log("Product ID: " + productID);
+                console.log("Product Name: " + productName);
+                console.log("Product Quantity: " + productQuantity);
+                console.log("Product Sold: " + productSold);
+                console.log("_________________________________________\n");
+
+            }
+            //for loop end here
+        });
+        //connection query end closure here
+        connection.end();
+    }
+    //else if end closure here
+    else if (userChoice.managerList === "Add to Inventory") {
+        inquirer.prompt([{
+            type: "input",
+            name: "userProductID",
+            message: "What is your Product Id ?"
+
+        }, {
+            type: "input",
+            name: "userQuantity",
+            message: "How many Quantity do you want to add?"
+        }]).then(function (answer) {
+
+            connection.query("SELECT item_id,product_name,stock_quantity FROM products_custmer where item_id ='" + answer.userProductID + "'", function (err, res) {
+                if (err) throw err;
+                var newStockQuantity = parseInt(res[0].stock_quantity) + parseInt(answer.userQuantity);
+                var productID = res[0].item_id;
+                var productName = res[0].product_name;
+                console.log("************************************\n");
+                console.log("Product ID: " + productID);
+                console.log("Product Name: " + productName);
+
+                connection.query("UPDATE products_custmer SET stock_quantity = '" + newStockQuantity + "' WHERE item_id = '" + answer.userProductID + "'", function (error, result) {
+                    if (error) throw error;
+                });
+                //update query end closure here
+                connection.query("SELECT stock_quantity FROM products_custmer where item_id = '" + answer.userProductID + "'", function (errors, resultstock) {
+                    if (errors) throw errors;
+                    var product_Newquantity = resultstock[0].stock_quantity;
+                    console.log("Product New Quantity: " + product_Newquantity);
+                    console.log("******************************************\n");
+                });
+                /// query stock end closure here
+            });
+            //inner query end closure here
+        });
+        //inquiere end here
+    }
+    //else if end closure here
+    else if (userChoice.managerList === "Add New Product") {
+        inquirer.prompt([{
+            type: "input",
+            name: "userProductName",
+            message: "What's your Product Name do you wnat to add?"
+        }, {
+            type: "input",
+            name: "userDepartName",
+            message: "What's your department name do you wnat to add?"
+        }, {
+            type: "input",
+            name: "userProductPrice",
+            message: "What's your price for your product(per unit) do you want to add?"
+        }, {
+            type: "input",
+            name: "userQuantity",
+            message: "What's your Product Stock Quantity do you wnat to add?"
+        }]).then(function (answer) {
+
+            connection.query("INSERT INTO products_custmer (product_name,department_name, price, stock_quantity,Sold_item) VALUES (?,?,?,?,?)", [answer.userProductName, answer.userDepartName, answer.userProductPrice, answer.userQuantity, 0], function (error, res) {
+                if (error) throw error;
+                var affectedrows = res.affectedRows;
+                console.log("************************************\n");
+                console.log("Your Product inserted into the Store");
+                console.log(affectedrows + "   Row inserted");
+                console.log("************************************\n")
+
+            });
+
+            connection.end();
+        }).catch(function (error) {
+            console.log(error);
+        });
+        //inquiere end closure here
+    }
+    //else if end closure here
+});
+//inquirer end closure here
--- customerview.js
+++ customerview.js
@@ -2,80 +2,76 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 //connection declare start
 var connection = mysql.createConnection({
-    host : "localhost",
-    port : 3306,
+    host: "localhost",
+    port: 3306,
     user: "root",
     password: "Sairakshna5*",
     database: "customerView_DB"
 });
 ///connection declare end closure
-connection.query("SELECT * FROM products_custmer", function(err, res){
+connection.query("SELECT * FROM products_custmer", function (err, res) {
     console.log(res[0]);
-    for (i=0; i< res.length;i++){
+    for (i = 0; i < res.length; i++) {
         //for loop start
         var id = res[i].item_id;
         var Name = res[i].product_name;
         var price = res[i].price;
 
         console.log("**********************");
-        console.log("Product ID:" +id);
-        console.log("Product Name: "+Name);
-        console.log("Product Price: "+price);
+        console.log("Product ID:" + id);
+        console.log("Product Name: " + Name);
+        console.log("Product Price: " + price);
         console.log("-----------------------\n");
-        } 
-        //for loop end   
-    
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "userProductID",
-                message: "What is your Product Id for your purchase?"
-                
-            },{
-                type: "input",
-                name: "userQuantity",
-                message: "How many Quantity need to purchase?"
-            }
-        ]).then(function(answer){
-        
-            console.log(answer.userProductID);
-        
-        connection.query("SELECT * FROM products_custmer where item_id = '"+answer.userProductID+"'",function(error,productresult){
-            if (error) throw error;  
-           var productID = productresult[0].item_id;
-           var productName = productresult[0].product_name;
-           var user_Quantity = answer.userQuantity;
-           console.log("********-------************");
-           console.log("Product ID: "+productID);
-           console.log("Product Name: "+productName);
-           console.log("USER PLACED QUANTITY: "+user_Quantity);
-           if((productresult[0].stock_quantity !== null) && (productresult[0].stock_quantity > 0)) {
-           var totalProductQuantity = productresult[0].stock_quantity - user_Quantity;
-           var totalProductPurchasePrice = user_Quantity * productresult[0].price;
-           console.log("Total Purchase: "+totalProductPurchasePrice);
+    }
+    //for loop end   
 
-        connection.query("UPDATE products_custmer SET stock_quantity = '"+totalProductQuantity+"' WHERE item_id = '"+answer.userProductID+"'",function(err,res){
-        if(err) throw err;
-        });
-    }
-    else{
-        console.log("Sorry Insufficient Stock Quanity");
-    }
-        
-    connection.end();
-        
+    inquirer.prompt([{
+        type: "input",
+        name: "userProductID",
+        message: "What is your Product Id for your purchase?"
+
+    }, {
+        type: "input",
+        name: "userQuantity",
+        message: "How many Quantity need to purchase?"
+    }]).then(function (answer) {
+
+        console.log(answer.userProductID);
+
+        connection.query("SELECT * FROM products_custmer where item_id = '" + answer.userProductID + "'", function (error, productresult) {
+            if (error) throw error;
+            var productID = productresult[0].item_id;
+            var productName = productresult[0].product_name;
+            var user_Quantity = answer.userQuantity;
+            console.log("********-------************");
+            console.log("Product ID: " + productID);
+            console.log("Product Name: " + productName);
+            console.log("USER PLACED QUANTITY: " + user_Quantity);
+            if ((productresult[0].stock_quantity !== null) && (productresult[0].stock_quantity > 0)) {
+                var totalProductQuantity = productresult[0].stock_quantity - user_Quantity;
+                var totalProductPurchasePrice = user_Quantity * productresult[0].price;
+                var sold_item = parseInt(productresult[0].Sold_item) + parseInt(user_Quantity);
+                console.log("Total Purchase: " + totalProductPurchasePrice);
+
+                connection.query("UPDATE products_custmer SET stock_quantity = '" + totalProductQuantity + "' , Sold_item = '" + sold_item + "' WHERE item_id = '" + answer.userProductID + "'", function (err, res) {
+                    if (err) throw err;
+                });
+
+
+            } else {
+                console.log("Sorry Insufficient Stock Quanity");
+            }
+
+            connection.end();
+
         });
         //query connection end closure here
-        
-            
-        }).catch(function(err){
-            console.log(err.response);
-        });   
-    
-    
+
+
+    }).catch(function (err) {
+        console.log(err.response);
+    });
+
+
 });
 ///connection query end-closure
-
-
-
-
